b785e4781f9ddf9f78e1d976336e663f80aa48be
asanka <asanka.gayashan@kochasoft.com>
2023-02-10 11:50:35 +0530

    bvbv

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ app.use(cors());
 // Routes
 app.use("/home", home);
 
-app.use('/solman-view', createProxyMiddleware({ target: 'https://solman.kochasoft.com/tac_alias_bsp/index.html?configId=1&DF_ALIAS=tac_alias_odata&FI_C_ALIAS=alias_dfl&RefreshTimer=30', changeOrigin: true }));
+app.use('/solman-view', createProxyMiddleware({ target: 'https://kqdtiwxo.kochasoft.com/tac_alias_bsp/index.html?configId=1&DF_ALIAS=tac_alias_odata&FI_C_ALIAS=alias_dfl&RefreshTimer=30', changeOrigin: true }));
 
-app.use('/test-demo', createProxyMiddleware({ target: 'https://www.w3schools.com/', changeOrigin: true }));
+app.use('/test-demo', createProxyMiddleware({ target: 'https://currentmillis.com/', changeOrigin: true }));
 
 
 // connection
