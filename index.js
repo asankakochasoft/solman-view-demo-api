ff56f0109efac95869cf0dc2aad1a3c1ec0c8a67
asanka <asanka.gayashan@kochasoft.com>
2023-02-10 08:11:03 +0530

    sd

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
   });
 
-// app.use('/solman-view', createProxyMiddleware({ target: 'https://solman.kochasoft.com/tac_alias_bsp/index.html?configId=1&DF_ALIAS=tac_alias_odata&FI_C_ALIAS=alias_dfl&RefreshTimer=30:27:61', changeOrigin: true }));
+app.use('/solman-view', createProxyMiddleware({ target: 'https://solman.kochasoft.com/tac_alias_bsp/index.html?configId=1&DF_ALIAS=tac_alias_odata&FI_C_ALIAS=alias_dfl&RefreshTimer=30:27:61', changeOrigin: true }));
 app.listen(3000);
